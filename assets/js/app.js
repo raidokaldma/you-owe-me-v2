bec6d113574ab2205ae4a23b7144a721d267cd7c
Raido Kaldma <raidokaldma@raido-mba.lan>
2013-12-07 14:21:21 +0200

    Initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,5 +14,5 @@ var YouOweMeApp = angular.module('youOweMe', ['ngAnimate', 'ngRoute'])
             });
     })
     .config(function ($httpProvider) {
-//        $httpProvider.defaults.headers.post = {'Content-Type': 'application/x-www-form-urlencoded'};
+        $httpProvider.defaults.headers.post = {'Content-Type': 'application/x-www-form-urlencoded'};
     });
